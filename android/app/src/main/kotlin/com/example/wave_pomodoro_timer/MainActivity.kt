48d69aafd0f06bf54b9b73eaac9b2dd2b01f7365
miloszratajczyk <milosz2002ratajczyk@gmail.com>
2020-04-08 01:27:46 +0200

    Add icons

--- android/app/src/main/kotlin/com/example/wave_pomodoro_timer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wave_pomodoro_timer/MainActivity.kt
@@ -1,18 +1,6 @@
 package com.example.wave_pomodoro_timer
 
-<<<<<<< HEAD
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-=======
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
->>>>>>> b09f3ef99d35d5538c708ebdf4094df9d4b08f46
 }
--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 #import "GeneratedPluginRegistrant.h"
-=======
-#import "GeneratedPluginRegistrant.h"
->>>>>>> b09f3ef99d35d5538c708ebdf4094df9d4b08f46
